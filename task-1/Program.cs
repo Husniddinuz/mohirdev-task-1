1f0e97c6d4bd8457ba6bf81deaaa0021b0610826
Husniddin Tugizov <husniddindev@gmail.com>
2024-05-15 02:29:53 +0500

    1-task commit

---
+++ task-1/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.Write("Radiusni kiriting: ");
+double radius = System.Convert.ToDouble(System.Console.ReadLine());
+
+double pi = System.Math.PI;
+double S = pi * System.Math.Pow(radius, 2);
+double L = 2 * pi * radius;
+
+System.Console.WriteLine($"S={S}, L={L}");
